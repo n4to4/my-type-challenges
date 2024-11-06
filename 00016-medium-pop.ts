61196f8206fc0496d2548115f216639607f74389
n4to4 <n4to4k@gmail.com>
2024-11-07 08:02:44 +0900

    type-challenges

--- 00016-medium-pop.ts
+++ 00016-medium-pop.ts
@@ -8,4 +8,4 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type Pop<T extends any[]> = T extends [...a: infer A, any] ? A : [];
+type Pop<T extends any[]> = T extends [...infer A, any] ? A : [];
