5f190f01a13b19ba8fc65ff8dfb5386ade1ecba3
n4to4 <n4to4k@gmail.com>
2024-11-24 09:25:18 +0900

    type-challenges

--- 00612-medium-kebabcase.ts
+++ 00612-medium-kebabcase.ts
@@ -14,4 +14,11 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type KebabCase<S> = any;
+type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S;
+
+type X = KebabCase<"FooBar">;
+type Y = Uncapitalize<"Foo">;
