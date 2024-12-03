80c6937745a70e12dd93b9905a68e4c0f77b5311
n4to4 <n4to4k@gmail.com>
2024-12-04 06:24:44 +0900

    type-challenges

--- 02757-medium-partialbykeys.ts
+++ 02757-medium-partialbykeys.ts
@@ -28,4 +28,16 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type PartialByKeys<T, K> = any;
+type PartialByKeys<T, K extends keyof T = never> = [K] extends [never]
+  ? Partial<T>
+  : Omit<
+      {
+        [p in keyof T as p extends K ? p : never]?: T[p];
+      } & {
+        [p in keyof T as p extends K ? never : p]: T[p];
+      },
+      never
+    >;
+
+type X = PartialByKeys<User, "name">;
+type Y = PartialByKeys<User>;
