aa8681a38b5b92c393056ff5bad124ef6e3700fa
n4to4 <n4to4k@gmail.com>
2024-12-10 08:29:47 +0900

    type-challenges

--- 03376-medium-inordertraversal.ts
+++ 03376-medium-inordertraversal.ts
@@ -55,6 +55,17 @@ interface TreeNode {
   left: TreeNode | null;
   right: TreeNode | null;
 }
-type InorderTraversal<T extends TreeNode | null> = T extends TreeNode
+type InorderTraversal_1<T extends TreeNode | null> = T extends TreeNode
   ? [...InorderTraversal<T["left"]>, T["val"], ...InorderTraversal<T["right"]>]
   : [];
+
+type InorderTraversal<
+  T extends TreeNode | null,
+  NT extends TreeNode = NonNullable<T>
+> = T extends null
+  ? []
+  : [
+      ...InorderTraversal<NT["left"]>,
+      NT["val"],
+      ...InorderTraversal<NT["right"]>
+    ];
