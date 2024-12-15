b6a31269c20333317ced59a5421bd1fcd18541f1
n4to4 <n4to4k@gmail.com>
2024-12-16 07:44:12 +0900

    type-challenges

--- 09286-medium-firstuniquecharindex.ts
+++ 09286-medium-firstuniquecharindex.ts
@@ -10,4 +10,22 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type FirstUniqueCharIndex<T extends string> = any;
+type Dup<
+  Char extends string,
+  Str extends string
+> = Str extends `${string}${Char}${string}` ? true : false;
+
+type FirstUniqueCharIndex<
+  T extends string,
+  Prefix extends string = "",
+  Idx extends any[] = []
+> = T extends `${infer First}${infer Rest}`
+  ? Rest extends `${string}${First}${string}`
+    ? FirstUniqueCharIndex<Rest, `${Prefix}${First}`, [...Idx, any]>
+    : Prefix extends `${string}${First}${string}`
+    ? FirstUniqueCharIndex<Rest, `${Prefix}`, [...Idx, any]>
+    : Idx["length"]
+  : -1;
+
+type X1 = "aa" extends `${string}a${string}` ? true : false;
+type X2 = FirstUniqueCharIndex<"aaa">;
