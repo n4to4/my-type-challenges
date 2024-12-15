af04888f24eabc8a4839172922dd15a80e79616f
n4to4 <n4to4k@gmail.com>
2024-12-16 07:22:04 +0900

    type-challenges

---
+++ 09142-medium-checkrepeatedchars.ts
@@ -0,0 +1,13 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+import { ExpectFalse, NotEqual } from "./test-utils";
+
+type cases = [
+  Expect<Equal<CheckRepeatedChars<"abc">, false>>,
+  Expect<Equal<CheckRepeatedChars<"abb">, true>>,
+  Expect<Equal<CheckRepeatedChars<"cbc">, true>>,
+  Expect<Equal<CheckRepeatedChars<"">, false>>
+];
+
+// ============= Your Code Here =============
+type CheckRepeatedChars<T extends string> = any;
