7f8a9b11ac96f47b7a872c64971bfc51657c95f9
n4to4 <n4to4k@gmail.com>
2024-12-16 08:03:56 +0900

    type-challenges

--- 09616-medium-parse-url-params.ts
+++ 09616-medium-parse-url-params.ts
@@ -11,4 +11,20 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type ParseUrlParams<T> = any;
+type Split<T extends string> = T extends `${infer First}/${infer Rest}`
+  ? [First, ...Split<Rest>]
+  : [T];
+
+type Extract<T extends string[]> = T extends [
+  infer Head,
+  ...infer Rest extends string[]
+]
+  ? Head extends `:${infer Param}`
+    ? Param | Extract<Rest>
+    : Extract<Rest>
+  : never;
+
+type ParseUrlParams<T extends string> = Extract<Split<T>>;
+
+type X1 = Split<"">;
+type X2 = Split<"posts/:id/:user/like">;
