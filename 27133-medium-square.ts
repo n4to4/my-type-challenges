cfd09844e402ca442ad32d73d76de4a415e357de
n4to4 <n4to4k@gmail.com>
2024-12-19 07:39:27 +0900

    type-challenges

--- 27133-medium-square.ts
+++ 27133-medium-square.ts
@@ -21,11 +21,36 @@ type Number<T extends number, Idx extends any[] = []> = Idx["length"] extends T
   ? Idx
   : Number<T, [...Idx, any]>;
 
-type Square<
+type Square1<
   N extends number,
   Arr extends any[] = Number<N>,
   Idx extends any[] = [],
   R extends any[] = []
 > = Idx["length"] extends N
   ? R["length"]
-  : Square<N, Arr, [...Idx, any], [...R, ...Arr]>;
+  : Square1<N, Arr, [...Idx, any], [...R, ...Arr]>;
+
+// ---
+
+type Abs<N extends number> = `${N}` extends `-${infer R extends number}`
+  ? R
+  : N;
+type SplitZeroes<
+  N extends number,
+  Z extends string = ""
+> = `${N}` extends `${infer N extends number}0`
+  ? SplitZeroes<N, `${Z}00`>
+  : [N, Z];
+type SquareTuple<
+  N extends number,
+  A extends any[] = [],
+  Acc extends any[] = []
+> = A["length"] extends N
+  ? [...A, ...Acc]
+  : SquareTuple<N, [1, ...A], [...A, ...A, ...Acc]>;
+
+type Square<
+  _N extends number,
+  N extends [number, string] = SplitZeroes<_N>,
+  U extends any[] = SquareTuple<Abs<N[0]>>
+> = `${U["length"]}${N[1]}` extends `${infer N extends number}` ? N : never;
