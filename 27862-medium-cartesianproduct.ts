e986c5764e914a211b349bb4b85cbecee03dfcb4
n4to4 <n4to4k@gmail.com>
2024-12-19 07:53:51 +0900

    type-challenges

--- 27862-medium-cartesianproduct.ts
+++ 27862-medium-cartesianproduct.ts
@@ -32,4 +32,8 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type CartesianProduct<T, U> = any;
+// Union<2 | 3> -> [2] | [3]
+type Union<T> = T extends T ? [T] : never;
+
+// [1, ...Union<2 | 3>] -> [1, 2] | [1, 3]
+type CartesianProduct<T, U> = T extends T ? [T, ...Union<U>] : never;
