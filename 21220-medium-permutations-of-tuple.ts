5c7c04f03fea213fd10fe52e7e2bd801f0bcc8c8
n4to4 <n4to4k@gmail.com>
2024-12-18 19:53:09 +0900

    type-challenges

--- 21220-medium-permutations-of-tuple.ts
+++ 21220-medium-permutations-of-tuple.ts
@@ -33,4 +33,13 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type PermutationsOfTuple<T extends unknown[]> = any;
+type Insert<T extends unknown[], U> = T extends [infer F, ...infer L]
+  ? [F, U, ...L] | [F, ...Insert<L, U>]
+  : [U];
+
+type PermutationsOfTuple<
+  T extends unknown[],
+  R extends unknown[] = []
+> = T extends [infer F, ...infer L]
+  ? PermutationsOfTuple<L, Insert<R, F> | [F, ...R]>
+  : R;
