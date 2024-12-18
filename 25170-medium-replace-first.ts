8fc98c884ce0f7137d1fcdc7f7aa798f2097a58e
n4to4 <n4to4k@gmail.com>
2024-12-18 20:01:18 +0900

    type-challenges

--- 25170-medium-replace-first.ts
+++ 25170-medium-replace-first.ts
@@ -12,4 +12,13 @@ type cases = [
 ]
 
 // ============= Your Code Here =============
-type ReplaceFirst<T extends readonly unknown[], S, R> = any;
+type ReplaceFirst<T extends readonly unknown[], S, R> = T extends [
+  infer Head,
+  ...infer Tail
+]
+  ? Head extends S
+    ? [R, ...Tail]
+    : [Head, ...ReplaceFirst<Tail, S, R>]
+  : [];
+
+type X1 = ReplaceFirst<[1, "two", 3], string, 2>;
