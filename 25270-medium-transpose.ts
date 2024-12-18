0d6412095d8be0c3ee789af562d72d9af99d0526
n4to4 <n4to4k@gmail.com>
2024-12-19 07:22:03 +0900

    type-challenges

--- 25270-medium-transpose.ts
+++ 25270-medium-transpose.ts
@@ -13,4 +13,8 @@ type cases = [
 ]
 
 // ============= Your Code Here =============
-type Transpose<M extends number[][]> = any;
+type Transpose<M extends number[][], R = M["length"] extends 0 ? [] : M[0]> = {
+  [X in keyof R]: {
+    [Y in keyof M]: X extends keyof M[Y] ? M[Y][X] : never;
+  };
+};
