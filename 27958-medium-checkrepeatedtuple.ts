80195190756ef9f723e214695fde93b348cdec18
n4to4 <n4to4k@gmail.com>
2024-12-20 07:11:30 +0900

    type-challenges

--- 27958-medium-checkrepeatedtuple.ts
+++ 27958-medium-checkrepeatedtuple.ts
@@ -1,6 +1,5 @@
 // ============= Test Cases =============
 import type { Equal, Expect } from "./test-utils";
-import { ExpectFalse, NotEqual } from "./test-utils";
 
 type cases = [
   Expect<Equal<CheckRepeatedTuple<[number, number, string, boolean]>, true>>,
