fe6fb33943b82711166310453c61458dc1ca3d72
n4to4 <n4to4k@gmail.com>
2024-12-20 06:31:21 +0900

    type-challenges

---
+++ 27932-medium-mergeall.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<MergeAll<[]>, {}>>,
+  Expect<Equal<MergeAll<[{ a: 1 }]>, { a: 1 }>>,
+  Expect<Equal<MergeAll<[{ a: string }, { a: string }]>, { a: string }>>,
+  Expect<Equal<MergeAll<[{}, { a: string }]>, { a: string }>>,
+  Expect<Equal<MergeAll<[{ a: 1 }, { c: 2 }]>, { a: 1; c: 2 }>>,
+  Expect<
+    Equal<
+      MergeAll<[{ a: 1; b: 2 }, { a: 2 }, { c: 3 }]>,
+      { a: 1 | 2; b: 2; c: 3 }
+    >
+  >,
+  Expect<Equal<MergeAll<[{ a: 1 }, { a: number }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: number }, { a: 1 }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>
+];
+
+// ============= Your Code Here =============
+type MergeAll<XS> = any;
